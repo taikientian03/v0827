97970b27515dd76c9570e27785f6b27cb2194293
fusatoshi3104 <satoshidat@gmail.com>
2020-08-27 11:20:21 +0900

    改造完成

--- v0827/Form1.cs
+++ v0827/Form1.cs
@@ -12,6 +12,8 @@ namespace v0827
 {
     public partial class Form1 : Form
     {
+        int vx = -10;
+        int vy = -10;
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +21,8 @@ namespace v0827
 
         private void label1_Click(object sender, EventArgs e)
         {
-           label1 left
+            vx = 0;
+            vy = 0;
         }
 
         private void label2_Click(object sender, EventArgs e)
@@ -29,8 +32,30 @@ namespace v0827
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            label1 left xy
-            label1
+            label1.Left += vx;
+            label1.Top += vy;
+
+            // 2次元クラスPoint型の変数mpを宣言
+            Point mp = MousePosition;
+
+            // mpに、マウスのフォーム座標を取り出す。
+            //// MousePositionにマウス座標のスクリーン左上からのX、Yが入っている。
+            //// PointToClient()を使うと、スクリーン座標を、フォーム座標に変換できる。
+            mp = PointToClient(mp);
+
+            // ラベルに座標を表示
+            //// 変換したフォーム座標は、mp.X、mp.Yで取り出せる。
+            label2.Text = "" + mp.X + "," + mp.Y;
+
+            if ((mp.X >= label1.Left)
+                && (mp.X < label1.Right)
+                && (mp.Y >= label1.Top)
+                && (mp.Y < label1.Bottom)
+                )
+            {
+                timer1.Enabled = false;
+            }
+
         }
     }
 }
